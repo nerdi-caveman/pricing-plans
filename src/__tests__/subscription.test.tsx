a8298441b42fb97c251af427f302842c54637ba7
Inioluwa Sogelola <inioluwwa4.is@gmail.com>
2020-05-05 16:44:26 +0100

    update test

--- src/__tests__/subscription.test.tsx
+++ src/__tests__/subscription.test.tsx
@@ -1,17 +1,20 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import nock from 'nock'; 
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import nock from "nock";
+import App from "../App";
 
-it('renders without crashing', () => { 
-   const scope = nock('http://myapi.com') 
-   .post('/subscription')
-   .reply(200, { success: true }, 
-   { 
-     'Access-Control-Allow-Origin': '*', 
-     'Content-type': 'application/json' 
-   }); 
-   const div = document.createElement('div');
-   ReactDOM.render(<App />, div);
-   ReactDOM.unmountComponentAtNode(div); 
+it("renders without crashing", () => {
+  describe("Should mock subscription pos request", () => {
+    const scope = nock("http://myapi.com").post("/subscription").reply(
+      200,
+      { success: true },
+      {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json",
+      }
+    );
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
